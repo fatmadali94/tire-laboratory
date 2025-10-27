4d84d5265d2d85ddd318088a118e61c53438d471
Moa Hamidi <fatmadali@gmail.com>
2025-10-27 16:44:29 +0330

    Add Docker setup for development and production

--- tire-lab-backend/index.js
+++ tire-lab-backend/index.js
@@ -1,6 +1,8 @@
 import express from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
+import { fileURLToPath } from 'url';
+import { dirname, join } from 'path';
 import authRoutes from './routes/authRoutes.js'
 import userRoutes from './routes/usersRoutes.js'
 import depositoryRecordsRoutes from './routes/depositoryRecordsRoutes.js'
@@ -13,8 +15,16 @@ import sizesRoutes from './routes/tire_data_routes/sizesRoutes.js'
 import customersRoutes from './routes/tire_data_routes/customersRoutes.js'
 import brandsRoutes from './routes/tire_data_routes/brandsRoutes.js'
 import retrievalFormRoutes from './routes/retrievalFormRoutes.js'
+// Get directory name in ES modules
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
-dotenv.config();
+// dotenv.config();
+if (process.env.NODE_ENV === 'production') {
+  dotenv.config({ path: join(__dirname, '.env.production') });
+} else {
+  dotenv.config({ path: join(__dirname, '.env.development') });
+}
 
 const app = express();
 
--- tire-lab-frontend/vite.config.js
+++ tire-lab-frontend/vite.config.js
@@ -1,3 +1,11 @@
+// // vite.config.js
+// import { defineConfig } from "vite";
+// import react from "@vitejs/plugin-react";
+
+// export default defineConfig({
+//   plugins: [react()],
+//   base: "/",
+// });
 // vite.config.js
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
@@ -5,4 +13,11 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   base: "/",
+  server: {
+    host: '0.0.0.0', // Important for Docker!
+    port: 5173,
+    watch: {
+      usePolling: true // Important for hot reload in Docker!
+    }
+  }
 });
